2a405f0732bcda9d4e6df6eed1a2de40d5da6063
Leonardo Puebla <leonardo38361@gmail.com>
2022-03-12 14:51:26 -0300

    Creación de evento del carrito. Colocación del Menu en Header y arreglos de menu.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "@styles/Header.scss";
 
 import menu from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
 import shoppingCart from "@icons/icon_shopping_cart.svg";
+import Menu from "@component/Menu";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
   return (
     <nav className="Header">
       <img src={menu} alt="menu" className="menu" />
@@ -34,13 +40,17 @@ const Header = () => {
       </div>
       <div className="navbar-right">
         <ul>
-          <li className="navbar-email">email@example.com</li>
+          <li className="navbar-email" onClick={handleToggle}>
+            email@example.com
+          </li>
           <li className="navbar-shopping-cart">
             <img src={shoppingCart} alt="shopping cart" />
             <div>2</div>
           </li>
         </ul>
       </div>
+      {toggle && <Menu />}
+      <Menu />
     </nav>
   );
 };
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/ProductItem.scss";
-
 import addToCart from "@icons/bt_add_to_cart.svg";
 
 const ProductItem = () => {
+  const [cart, setCart] = useState([]);
+  const handleClick = () => {
+    setCart([]);
+  };
+
   return (
     <div className="ProductItem">
       <img
@@ -15,7 +19,7 @@ const ProductItem = () => {
           <p>$120,00</p>
           <p>Painting</p>
         </div>
-        <figure>
+        <figure onClick={handleClick}>
           <img src={addToCart} alt="add to cart" />
         </figure>
       </div>
