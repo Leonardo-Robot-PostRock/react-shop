dbae5bdb03fe676521434dc79754a0c2313b9325
Leonardo Puebla <leonardo38361@gmail.com>
2022-03-13 00:52:20 -0300

    Se crea evento de formulario con useRef

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,7 +49,6 @@ const Header = () => {
         </ul>
       </div>
       {toggle && <Menu />}
-      <Menu />
     </nav>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,49 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../styles/Login.scss";
 
-import logo from '@logos/logo_yard_sale.svg';
+import logo from "@logos/logo_yard_sale.svg";
 
-const Login = () => {
+const Login = (event) => {
+  const form = useRef(null);
+
+  const handleSubmit = (event) => {
+		event.preventDefault();
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log(data);
+  };
   return (
     <div className="Login">
       <div className="Login-container">
         <img src={logo} alt="logo" className="logo" />
-        <form action="/" className="form">
-          <label for="email" className="label">
+        <form action="/" className="form" ref={form}>
+          <label htmlFor="email" className="label">
             Email address
           </label>
           <input
             type="text "
-            id="password"
+            name="email"
             placeholder="mail@example.com"
             className="input input-email"
           />
-          <label for="password" className="label">
+          <label htmlFor="password" className="label">
             Password
           </label>
           <input
             type="password"
-            id="password"
+            name="password"
             placeholder="*********"
             className="input input-password"
           />
-          <input
-            type="submit"
-            value="Log in"
+          <button
+            onClick={handleSubmit}
             className="primary-button login-button"
-          />
+          >
+            Log in
+          </button>
           <a href="/">Forgot my password</a>
         </form>
         <button className="secondary-button singUp-button">Sing up</button>
