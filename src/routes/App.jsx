c53416632de9d38c92a21f9035cead824a434bb0
Leonardo Puebla <leonardo38361@gmail.com>
2022-03-15 01:34:02 -0300

    Se pone en práctica react context para que al crear los eventos se puedan llevar con más facilidad la info, de padre a hijo

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -12,25 +12,42 @@ import MyAccount from "../pages/MyAccount";
 import Orders from "../pages/Orders";
 import NotFound from "../pages/NotFound";
 import "../styles/global.css";
+import AppContext from "../context/AppContext";
+import useInitialState from "../hooks/useInitialState";
 
 const App = () => {
+  const initialState = useInitialState();
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/create-account" element={<CreateNewAccount />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/my-account" element={<MyAccount />} />
-          <Route exact path="/recovery-password" element={<PasswordRecovery />} />
-          <Route exact path="/mail-sent" element={<MailSent />} />
-          <Route exact path="/create-new-password" element={<CreateNewPassword />} />
-          <Route exact path="/orders" element={<Orders />} />
-          <Route exact path="/checkout" element={<Checkout />} />
-          <Route exact path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/create-account"
+              element={<CreateNewAccount />}
+            />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/my-account" element={<MyAccount />} />
+            <Route
+              exact
+              path="/recovery-password"
+              element={<PasswordRecovery />}
+            />
+            <Route exact path="/mail-sent" element={<MailSent />} />
+            <Route
+              exact
+              path="/create-new-password"
+              element={<CreateNewPassword />}
+            />
+            <Route exact path="/orders" element={<Orders />} />
+            <Route exact path="/checkout" element={<Checkout />} />
+            <Route exact path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 };
 
