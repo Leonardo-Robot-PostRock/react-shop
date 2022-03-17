f636b0b756914e4ea510b0e5f78b76784269c122
Leonardo Puebla <leonardo38361@gmail.com>
2022-03-16 23:18:21 -0300

    cambio en publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'react-shop/bundle.js',
-        publicPath: './'
+        publicPath: '/'
     },
     mode: 'development',
     resolve: {
